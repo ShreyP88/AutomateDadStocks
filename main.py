24d903ecf6a89aa1f2b1c65cacd94596db9a2ce3
Shreyas Pant <shrey.pant88@gmail.com>
2021-01-07 14:14:09 -0500

    first setup

---
+++ main.py
@@ -0,0 +1,72 @@
+# Process:
+# Import necessary libraries
+# Get spreadsheet location and name
+# Store values in variables
+# Make the input text of the name of the stock uppercase 
+# Present a small menu which will allow to either edit, add, or view
+# Create functions for each with proper logic
+
+import openpyxl
+import os
+
+def setup():
+    print("I RAN!")
+    #directory = input("Please enter the location of the spreadsheet. Example: ")
+    directory = "C:\\Users\\shrey\\Desktop"
+    directory = directory.lower()
+    os.chdir(directory)
+    #spreadname = input("Please enter the name of the spreadsheet. Example: stocks.xlsx")
+    spreadname = "Stocks.xlsx"
+    workbook = openpyxl.load_workbook(spreadname)
+    sheet_Arr = workbook.sheetnames
+    answer = ''
+    sheetname = ''
+    while answer != 'Y':
+        for x in sheet_Arr:
+            answer = input(x + ": Is this the sheetname which you are trying to access? (Y/N)")
+            answer = answer.upper()
+            if (answer == 'Y'):
+                sheetname = sheet_Arr[0]
+                break
+    print(sheetname)
+    sheet = workbook[sheetname]
+    choose(sheet)
+
+def choose(sheet):
+    choice = ''
+    while choice != 'V' or choice != 'E' or choice != 'A': 
+        choice = input("Would you like to view (V), edit (E), or add (A) a stock?")
+        choice = choice.upper()
+        if choice == 'V':
+            showStock(sheet)
+            break
+        elif choice == 'E':
+            editStock(sheet)
+            break
+        elif choice == 'A':
+            addStock(sheet)
+            break
+        else:
+            print("Please enter a valid option") 
+    
+
+def showStock(sheet):
+    stockName = input("What is the name of the stock which you are trying to view? Example: TSLA")
+    stockRow = 0
+    for i in range(1, 3):
+        if sheet.cell(row=i, column = 1).value == stockName:
+            stockRow = i
+    print("Here is the info for " + str(stockName))
+    for i in range(1, 10):
+        print(sheet.cell(row = stockRow, column = i).value)
+    
+def editStock(sheet):
+    return None
+
+def addStock(sheet):
+    return None
+    
+def main():
+    setup()
+
+main()
